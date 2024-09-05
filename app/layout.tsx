1a1d77564b507c3fc4f75907af2e50663251b542
Adhitya-S <adhityavalli@gmail.com>
2024-09-05 20:14:57 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import NavBar from '@/components/NavBar';
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "Travel",
-  description: "Travel App For Camping",
+  title: "Superlink",
+  description: "A Massive Link For Camping",
 };
 
 export default function RootLayout({
